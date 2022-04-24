873c2b5792ddf8d9373c11e92fe83f15f2f774f6
Roman <77742141+rumpear@users.noreply.github.com>
2022-04-24 19:06:26 +0300

    added genres on main

--- src/js/handlers/handleCategory.js
+++ src/js/handlers/handleCategory.js
@@ -1,11 +1,8 @@
 import { store } from '../store';
-
 import { handleGallery } from './handleGallery';
 
 export const handleCategory = (e) => {
   const { value } = e.target;
-  // console.log(e.target);
-  // console.log(value);
 
   store.category = value;
 
@@ -16,34 +13,26 @@ store.refs.categoryButtons.forEach((input) =>
   input.addEventListener('change', handleCategory)
 );
 
-export const handleGenre = (e) => {
-  store.mode = 'genres';
-  const { value } = e.target;
-  store.genreId = value;
+// export const handleGenre = (e) => {
+//   store.mode = 'genres';
+//   const { value } = e.target;
+//   store.genreId = value;
 
-  store.refs.categoriesRoot.classList.add('is-closed');
-  store.refs.typeRoot.classList.remove('is-closed');
+//   store.refs.categoriesRoot.classList.add('is-closed');
+//   store.refs.typeRoot.classList.remove('is-closed');
 
-  console.log(e.target);
-  console.log(value);
+//   console.log(e.target);
+//   console.log(value);
 
-  if (store.category === 'all') {
-    store.category = 'movie';
-  }
+//   if (store.category === 'all') {
+//     store.category = 'movie';
+//   }
 
-  console.log('handleGenre', value);
+//   console.log('handleGenre', value);
 
-  handleGallery();
-};
+//   handleGallery();
+// };
 
-// const genreButtons = document.querySelectorAll('[name="genres"]');
-
-store.refs.genreButtons.forEach((input) =>
-  input.addEventListener('change', handleGenre)
-);
-// genreButtons.forEach((input) => (input.checked = false));
-
-// const comedy = document.getElementById('Comedy');
-// comedy.checked = false;
-
-// document.getElementById('Comedy').checked = false;
+// store.refs.genreButtons.forEach((input) =>
+//   input.addEventListener('change', handleGenre)
+// );

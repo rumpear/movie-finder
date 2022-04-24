32544f691dcf79d6f8b78cd42fee91ba99eb5c81
Roman <77742141+rumpear@users.noreply.github.com>
2022-04-24 19:06:16 +0300

    added genres on main

---
+++ src/js/handlers/genres/handleGenre.js
@@ -0,0 +1,28 @@
+import { store } from '../../store';
+import { handleGallery } from '../handleGallery';
+
+export const handleGenre = (e) => {
+  // const { mode, genreId, refs, category } = store;
+
+  store.mode = 'genres';
+  const { value } = e.target;
+  store.genreId = value;
+
+  store.refs.categoriesRoot.classList.add('is-closed');
+  store.refs.typeRoot.classList.remove('is-closed');
+
+  console.log(e.target);
+  console.log(value);
+
+  if (store.category === 'all') {
+    store.category = 'movie';
+  }
+
+  console.log('handleGenre', value);
+
+  handleGallery();
+};
+
+store.refs.genreButtons.forEach((input) =>
+  input.addEventListener('change', handleGenre)
+);

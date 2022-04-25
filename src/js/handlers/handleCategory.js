9b1f393400fbe4242d43383a69a460efcb8fa837
Roman <77742141+rumpear@users.noreply.github.com>
2022-04-25 22:57:02 +0300

    Update handleCategory.js

--- src/js/handlers/handleCategory.js
+++ src/js/handlers/handleCategory.js
@@ -12,27 +12,3 @@ export const handleCategory = (e) => {
 store.refs.categoryButtons.forEach((input) =>
   input.addEventListener('change', handleCategory)
 );
-
-// export const handleGenre = (e) => {
-//   store.mode = 'genres';
-//   const { value } = e.target;
-//   store.genreId = value;
-
-//   store.refs.categoriesRoot.classList.add('is-closed');
-//   store.refs.typeRoot.classList.remove('is-closed');
-
-//   console.log(e.target);
-//   console.log(value);
-
-//   if (store.category === 'all') {
-//     store.category = 'movie';
-//   }
-
-//   console.log('handleGenre', value);
-
-//   handleGallery();
-// };
-
-// store.refs.genreButtons.forEach((input) =>
-//   input.addEventListener('change', handleGenre)
-// );

e3b7ab4d81d9c50a0f350e20ee55e4be3b26f0e1
Roman <77742141+rumpear@users.noreply.github.com>
2022-04-26 15:46:53 +0300

    Update parseYear.js

--- src/js/utils/parseYear.js
+++ src/js/utils/parseYear.js
@@ -2,5 +2,5 @@ export const parseYear = (fromDate, ToDate) => {
   if (ToDate) return `${parseInt(fromDate)} - ${parseInt(ToDate)}`;
 
   if (fromDate) return `${parseInt(fromDate)}`;
-  else return ' - ';
+  else return 'Year unknown';
 };
